859b03f3db31807c97ffc21a4f7c4f9169b93d3e
Joshua Chow <thejoshchow@gmail.com>
2021-01-12 05:09:36 -0800

    no changes

--- app/flask.py
+++ app/flask.py
@@ -5,11 +5,9 @@ from model_data.predict import predict
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('home.html')
-
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
@@ -22,13 +20,7 @@ def _recommend_vid(style):
     user_data = request.json
     style = user_data['style']
     data = predict(style)
-    # return jsonify({'result': game_list})
-    data = data.values.tolist()
-    return render_template('recommend.html')
-
-def func():
-    pass
-
+    return render_template('recommend.html', data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)

6781f359980139c8c2ee5fa745cb51c87cd94e5e
Joshua Chow <thejoshchow@gmail.com>
2021-01-12 13:58:44 -0800

    first iteration of working application

--- app/model_data/predict.py
+++ app/model_data/predict.py
@@ -7,13 +7,13 @@ from gensim import models, similarities
 
 class predict():
     def __init__(self, query):
-        self.df = pd.read_csv('../../data/yoga.csv')
-        with open('dictionary', 'rb') as fp:
+        self.df = pd.read_csv('model_data/yoga.csv')
+        with open('model_data/dictionary', 'rb') as fp:
             self.dictionary = pickle.load(fp)
-        with open('corpus', 'rb') as fp:
+        with open('model_data/corpus', 'rb') as fp:
             self.corpus = pickle.load(fp)
-        self.index = similarities.MatrixSimilarity.load('index')
-        self.lda = models.LdaModel.load('lda_model')
+        self.index = similarities.MatrixSimilarity.load('model_data/index')
+        self.lda = models.LdaModel.load('model_data/lda_model')
         self.query = query
         self.vec_bows = self.dictionary.doc2bow(self.query.lower().split())
         self.vec_lda = self.lda[self.vec_bows]
@@ -23,18 +23,33 @@ class predict():
         sims = self.index[self.query_vector]
         return sims
 
-    def vid_ids(self):
+    # def vid_embed(self):
+    #     sims = self.get_similarities()
+    #     sims = sorted(enumerate(sims), key=lambda item: -item[1])
+    #     ids = [(doc_position, doc_score) for doc_position, doc_score in sims[0:9]]
+    #     vid_ids = [self.df.iloc[index[0]]['id'] for index in ids]
+    #     links = [f'https://www.youtube.com/watch?v={id}' for id in vid_ids]
+    #     return embed_table
+
+    def vid_table(self):
         sims = self.get_similarities()
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
-        ids = [(doc_position, doc_score) for doc_position, doc_score in sims]
-        vid_ids = [self.df.iloc[index[0]]['id'] for index in ids]
-        links = [f'https://www.youtube.com/watch?v={id}' for id in vid_ids]
-        return links
+        ids = [(doc_position, doc_score) for doc_position, doc_score in sims[0:10]]
+        # vid_ids = [self.df.iloc[index[0]]['id'] for index in ids]
+        to_html = pd.DataFrame(columns=self.df.columns)
+        for id in ids:
+            to_html = to_html.append(self.df.iloc[id[0]])
+        heading = ['title', 'description', 'id']
+        for i in to_html.columns:
+            if i not in heading:
+                to_html.drop(columns=i, inplace=True)
+        to_html['id'] = to_html['id'].apply(lambda x: f'https://www.youtube.com/watch?v={x}')
+        return to_html
 
 def main():
     query = input('what type of yoga are you looking for? ')
     out = predict(query)
-    print(out.vid_ids())
+    print(out.vid_table())
         
 
 if __name__ == '__main__':
